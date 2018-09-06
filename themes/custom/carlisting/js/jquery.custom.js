0be196f9f01e663f9b743445b4093e2f335e402a
shahiq63 <syed.shahiq@square63.com>
2018-09-06 11:39:33 +0500

    Reset button added on car ads page and Price,Year fields are inline

--- themes/custom/carlisting/js/jquery.custom.js
+++ themes/custom/carlisting/js/jquery.custom.js
@@ -1,3 +1,7 @@
 jQuery(function($) {
   $(".inline-element").parent("div").addClass("inline-elements");
+  $('form').find('.form-item-field-price-value-min').addClass("inline-elements");
+  $('form').find('.form-item-field-price-value-max').addClass("inline-elements");
+  $('form').find('.form-item-field-year-value-min').addClass("inline-elements");
+  $('form').find('.form-item-field-year-value-max').addClass("inline-elements");
 });
